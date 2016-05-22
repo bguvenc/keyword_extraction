aa30e6f8c7b4163df09cea00c8b5657e1988f310
bguvenc <betulguvenc@gmail.com>
2016-05-22 22:13:20 +0300

    Update get_w2v.py

--- get_w2v.py
+++ get_w2v.py
@@ -1,51 +1,51 @@
 import re
 import nltk
-from nltk.corpus import stopwords 
 import nltk.data
-import gensim, logging
 from gensim.models import word2vec
-from gensim.models import Word2Vec
-import numpy as np
-import logging
-#logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
-#level=logging.INFO)
 
-with open("dream_luther.txt") as file: 
+
+with open("Wikipedia_Text.txt") as file:
     text_review = file.read()
 
 # Function to convert a document to a sequence of words,
-def text_to_wordlist( text, remove_stopwords=False ):
-   # 2. Remove non-letters
-    review_text = re.sub("[^a-zA-Z]"," ", text)
+def text_to_wordlist(text, remove_stopwords=False):
+    # 2. Remove non-letters
+    review_text = re.sub("[^a-zA-Z]", " ", text)
+
     # 3. Convert words to lower case and split them
     words = review_text.lower().split()
-    return(words)
-   
-tokenizer = nltk.data.load('tokenizers/punkt/english.pickle') 
+
+    return (words)
+
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
 # Define a function to split a text into parsed sentences
-def text_to_sentences( text, tokenizer, remove_stopwords=False ):
-
+def text_to_sentences(text, tokenizer, remove_stopwords=False):
+    # Function to split a text into parsed sentences. Returns a 
+    # list of sentences, where each sentence is a list of words
+    #
     # 1. Use the NLTK tokenizer to split the paragraph into sentences
-    review = text.decode('utf-8').replace(u'\u014c\u0106\u014d','-')
+    review = text.decode('utf-8').replace(u'\u014c\u0106\u014d', '-')
     raw_sentences = tokenizer.tokenize(review.strip())
-
+    #
+    # 2. Loop over each sentence
     sentences = []
     for sentence in raw_sentences:
+        # If a sentence is empty, skip it
         if len(sentence) > 0:
             # Otherwise, call review_to_wordlist to get a list of words
-            sentences.append( text_to_wordlist( sentence, remove_stopwords = False ))
+            sentences.append(text_to_wordlist(sentence, remove_stopwords=False))
 
     return sentences
 
-sentences = [] 
+sentences = []
 
-#Parsing sentences from training set
+# Parsing sentences from training set
 for review in text_review:
-    sentences += text_to_sentences(text_review, tokenizer, remove_stopwords=False )
+    sentences += text_to_sentences(text_review, tokenizer, remove_stopwords=False)
 
 num_features = 100
-model = word2vec.Word2Vec(sentences, size = num_features, window=5, min_count=0)
+model = word2vec.Word2Vec(sentences, size=num_features, window=5, min_count=0)
 model_name = "w2v"
 model.save(model_name)
 # If you don't plan to train the model any further, calling 
@@ -55,10 +55,10 @@ model = word2vec.Word2Vec.load(model_name)
 
 print type(model.syn0)
 print model.syn0.shape
-#it gives vector representation of a word
-#print model["and"]
+# it gives vector representation of a word
+print model["and"]
 
-#bigram_transformer = gensim.models.Phrases(sentences)
-#model = word2vec.Word2Vec(bigram_transformed[sentences], size=100, ...)
+# bigram_transformer = gensim.models.Phrases(sentences)
+# model = word2vec.Word2Vec(bigram_transformed[sentences], size=100, ...)
 
-#print model.similarity('suicide','hospital')
+print model.similarity('word_1','word_2')
